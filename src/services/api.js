976a3e190f6f36ea00af4e6dda8c1b7bff1283f2
Kumorai9-star <kumorai9@gmail.com>
2026-07-09 06:37:22 +0545

    api change

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // ⚡ Ensure this base URL matches your backend port exactly!
 const API = axios.create({
-  baseURL: 'http://127.0.0.1:5000/api', // Use the exact IP your backend is running on
+  baseURL: 'https://cafe-backend-mjv4.onrender.com/api',  // Use the exact IP your backend is running on
 });
 
 export const createReservation = (data) => API.post('/reservation', data);
